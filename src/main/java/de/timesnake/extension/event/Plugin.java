3fc10ef1fc70a920d93a9786520f91b228a51e1b
funzter <cavehive@protonmail.com>
2022-11-11 20:31:15 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/extension/event/Plugin.java
+++ src/main/java/de/timesnake/extension/event/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * extension-event.main
+ * timesnake.extension-event.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,13 +18,18 @@
 
 package de.timesnake.extension.event;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-    public static final Plugin EASTER = new Plugin("Easter", "XEE");
-    public static final Plugin BIRTHDAY = new Plugin("Birthday", "XEB");
-    public static final Plugin CHRISTMAS = new Plugin("Christmas", "XEC");
+    public static final Plugin EASTER = new Plugin("Easter", "XEE", LogHelper.getLogger("Easter", Level.INFO));
+    public static final Plugin BIRTHDAY = new Plugin("Birthday", "XEB", LogHelper.getLogger("Birthday", Level.INFO));
+    public static final Plugin CHRISTMAS = new Plugin("Christmas", "XEC", LogHelper.getLogger("Christmas", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
