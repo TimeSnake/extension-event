f20a5e6f9bbcd3256de49d6a5ca62685cfb1068c
funzter <cavehive@protonmail.com>
2023-02-10 17:48:38 +0100

    updated for changed inventory api

--- src/main/java/de/timesnake/extension/event/birthday/BirthdayEvent.java
+++ src/main/java/de/timesnake/extension/event/birthday/BirthdayEvent.java
@@ -5,7 +5,7 @@
 package de.timesnake.extension.event.birthday;
 
 import de.timesnake.basic.bukkit.util.Server;
-import de.timesnake.basic.bukkit.util.user.ExItemStack;
+import de.timesnake.basic.bukkit.util.user.inventory.ExItemStack;
 import de.timesnake.basic.bukkit.util.user.User;
 import de.timesnake.basic.bukkit.util.user.event.UserJoinEvent;
 import de.timesnake.basic.bukkit.util.user.event.UserQuitEvent;
